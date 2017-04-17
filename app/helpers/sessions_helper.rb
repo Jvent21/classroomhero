3787b312cbcf6b78f7cee3671829ab1eaa58813e
Jason Ventura <jasonv@email.uscupstate.edu>
2017-04-17 17:02:38 +0000

    Finished Index and Admins

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,15 +3,17 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
-
-  # Remembers a user in a persistent session.
+    
     def remember(user)
         user.remember
         cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
     end
-
-  # Returns the current logged-in user (if any).
+    
+    def current_user?(user)
+      user == current_user
+    end
+    
     def current_user
         if (user_id = session[:user_id])
           @current_user ||= User.find_by(id: user_id)
@@ -23,8 +25,7 @@ module SessionsHelper
         end
         end
     end
-
-  # Returns true if the user is logged in, false otherwise.
+    
     def logged_in?
         !current_user.nil?
     end
@@ -41,4 +42,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
